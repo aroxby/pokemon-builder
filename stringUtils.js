c644414e250d00df88d4b73481ca5cdedbe9d294
Andy Roxby <aroxby@users.noreply.github.com>
2022-06-02 15:24:42 -0400

    Remove debug logging

--- stringUtils.js
+++ stringUtils.js
@@ -1,5 +1,4 @@
 function padString(str, desiredLength, padding = '\0') {
-    console.log(str, desiredLength, padding);
     const newStr = str + padding.repeat(
         Math.ceil(Math.max(desiredLength - str.length, 0) / padding.length)
     );
